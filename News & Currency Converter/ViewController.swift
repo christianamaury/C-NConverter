4296c7d7da231a9f6c68d41b04be24cabb08162e
christianamaury <chris.amaury@hotmail.com>
2019-09-06 00:05:32 -0700

    Current Version

--- News & Currency Converter/ViewController.swift
+++ News & Currency Converter/ViewController.swift
@@ -30,8 +30,7 @@ class ViewController: UIViewController, UIPickerViewDelegate,
     @IBOutlet weak var pickerView2: UIPickerView!
     
     //fake data..
-    //var age = ["20-30", "50-60", "70-100"]
-    
+    var age = ["20-30", "50-60", "70-100"]
     var genderType = ["male", "female", "hybrid"]
     
     //Testing with real data..
